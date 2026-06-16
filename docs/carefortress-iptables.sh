05db338bc8d10694d4fe1d99f07030cb07ce7d4b
James Smith <Smithjamesd89@live.com>
2026-06-16 10:23:53 +0000

    feat: NTP hardening -- chrony NTS upstream, VM 102 as internal NTP server, guest VMs syncing, external NTP blocked

--- docs/carefortress-iptables.sh
+++ docs/carefortress-iptables.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# CareFortress - block guest VM access to host services
+# CareFortress Host Firewall Rules
+
 for BRIDGE in virbr1 virbr2 virbr3; do
+    # Block guest VMs from SSHing to hypervisor host
     iptables -I INPUT -i $BRIDGE -p tcp --dport 22 -j DROP
+    # Block guest VMs from reaching external DNS
     iptables -I INPUT -i $BRIDGE -p tcp --dport 53 -j DROP
     iptables -I INPUT -i $BRIDGE -p udp --dport 53 -j DROP
+    # Allow NTP queries from guest VMs to VM 102 (chrony serves on 0.0.0.0:123)
+    iptables -I INPUT -i $BRIDGE -p udp --dport 123 -j ACCEPT
+    # Block guest VMs from forwarding NTP to external servers
+    iptables -I FORWARD -i $BRIDGE -p udp --dport 123 -j DROP
 done
